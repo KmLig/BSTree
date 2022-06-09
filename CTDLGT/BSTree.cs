296cb81fed5cccde60bff92396133fc540e40656
KmLig <phamxuankhiem1102@gmail.com>
2022-06-10 01:16:48 +0700

    Binary Search Tree

---
+++ CTDLGT/BSTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTDLGT
+{
+    public class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+        public Node(int a_value)
+        {
+            value = a_value;
+            left = null;
+            right = null;
+        }
+    }
+
+    public class BSTree
+    {
+        Node root;
+        public BSTree()
+        {
+            root = null;
+        }
+        private void Add(ref Node _root, int value)
+        {
+            Node newNode = new Node(value);
+            if (_root == null)
+            {
+                _root = newNode;
+                return;
+            }
+            if (value < _root.value)
+            {
+                Add(ref _root.left, value);
+                return;
+            }
+            Add(ref _root.right, value);
+        }
+        private void InOrder(Node _root)
+        {
+            if (_root == null)
+            {
+                return;
+            }
+            InOrder(_root.left);
+            Console.WriteLine(_root.value);
+            InOrder(_root.right);
+        }
+        public void InOrder()
+        {
+            InOrder(root);
+        }
+        public void Add(int value)
+        {
+            Add(ref root, value);
+        }
+    }
+}
---
+++ CTDLGT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTDLGT
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BSTree newTree = new BSTree();
+            newTree.Add(5);
+            newTree.Add(12);
+            newTree.Add(3);
+            newTree.InOrder();
+        }
+    }
+}
